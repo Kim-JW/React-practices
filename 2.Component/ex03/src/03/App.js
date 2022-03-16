cca3838c31a7a56253bb82828f6bd7c77e15a916
Kim-JW <bbam03@naver.com>
2022-03-16 10:48:38 +0900

    React Event function

---
+++ 2.Component/ex03/src/03/App.js
@@ -0,0 +1,43 @@
+import React, {Fragment} from 'react';
+
+export default function App() {
+
+    const onChangeMessageInput = function(event) {
+        console.log(event.target.name, ':', event.target.value);
+    }
+
+    const onAddFormSubmit = function(event) {
+
+        // 페이지 이동을 막아두고, Ajax를 사용.
+        event.preventDefault();
+        console.log(event.target.name, ':', 'Ajax로 등록할 겁니다.');
+    }
+
+    return (
+        <Fragment>
+            <h2>ex03 - SyntheticEvent</h2>
+            <p>
+                Native DOM Event 객체를 Wrapper 하고 있다.<br/>
+                Native 이벤트 객체와 사용하는 방식이 같다(인테페이스 동일)
+            </p>
+            <form
+                name='addForm'
+                method='post'
+                action='/do/not/go'
+                onSubmit={onAddFormSubmit}
+                >
+                <input
+                    type='text'
+                    name='message'
+                    placeholder='메세지를 입력 하세요'
+                    onChange={onChangeMessageInput}
+                    />
+                <br/>
+                <br/>
+                <input
+                    type='submit'
+                    value='등록' />
+            </form>
+        </Fragment>
+    );
+}
