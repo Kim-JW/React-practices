b0b0d12f737f9e34e859f6e50eca9f962421469c
Kim-JW <bbam03@naver.com>
2022-03-16 11:38:55 +0900

    React Mouse Events

--- 2.Component/ex03/src/04/App.js
+++ 2.Component/ex03/src/04/App.js
@@ -3,12 +3,31 @@ import logo from '../assets/images/react-logo.png';
 
 export default function App() {
    // const imageRef = useRef(null);
+   // 후크 함수
+    const imgRef = useRef(null);
 
     const onKeyPressInput = e => {
         if(e.key == 'Enter'){
             console.log(e.target.value);
         }
     }
+
+    const onFocusInput = e => console.log('focused');
+    const onBlurInput = e => console.log('blur');
+    const onMouseOverImage = e => console.log('mouseover', `x=${e.clientX}, y=${e.clientY}`);
+    const onMouseOutImage = e => console.log('mouseout', `x=${e.clientX}, y=${e.clientY}`);
+    const onMouseMoveImage = e => {
+        // const offsetTop = $(imgRef). - jQuery 설치해서 할수도 잇다.
+        const offsetTop = imgRef.current.offsetTop;
+        const offsetLeft = imgRef.current.offsetLeft;
+
+        console.log('mouseout', `x=${e.clientX-offsetLeft}, y=${e.clientY-offsetTop}`);
+    }
+
+    const onMouseDownImage = e => console.log('mousedown', `x=${e.clientX}, y=${e.clientY}`);
+    const onMouseUpImage = e => console.log('mouseup', `x=${e.clientX}, y=${e.clientY}`);
+    const onClickImage = e => console.log('click', `x=${e.clientX}, y=${e.clientY}`);
+    const onDoubleClickImage = e => console.log('dbclick', `x=${e.clientX}, y=${e.clientY}`);
 
     return (
         <Fragment>
@@ -17,17 +36,26 @@ export default function App() {
                 type='text'
                 placeholder='메세지를 입력 하세요'
                 onKeyPress={ onKeyPressInput }
-                
+                onFocus={onFocusInput}
+                onBlur={onBlurInput}
                 />
                 <br/>
                 <br/>
             <img
+                ref = { imgRef }
                 style={ {
                     cursor: 'pointer',
                     width: 190,
                     border: '1px solid #ccc'
                 } }
                 src={ logo }
+                onMouseOver={onMouseOverImage}
+                onMouseOut={onMouseOutImage}
+                onMouseMove={onMouseMoveImage}
+                onMouseDown={onMouseDownImage}
+                onMouseUp={onMouseUpImage}
+                onClick={onClickImage}
+                onDoubleClick={onDoubleClickImage}
                />
         </Fragment>
     );
