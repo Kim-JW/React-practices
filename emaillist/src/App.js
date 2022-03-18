933530388893231d9f51ec2b905b972776f5dcc4
Kim-JW <bbam03@naver.com>
2022-03-18 12:54:14 +0900

    emaillist scss component

--- emaillist/src/App.js
+++ emaillist/src/App.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import './assets/scss/App.scss';
+import RegisterForm from './RegisterForm';
+import SearchBar from './SearchBar';
+import Emaillist from './Emaillist';
+import data from './assets/json/data.json'
+
+//global 안 썼을 경우 - 해싱
+// import styles from './assets/scss/App.scss';
+// styles.App 
 
 const App = () => {
+  
   return (
-    <div>Emaillist</div>
+    <div className={'App'}>
+        <RegisterForm />
+        <SearchBar />
+        <Emaillist emails={data}/>
+    </div>
   )
 }
 
---
+++ emaillist/src/Emaillist.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from './assets/scss/Emaillist.scss'
+import Email from './Email'
+
+const Emaillist = ({emails}) => {
+  return (
+    <ul className={styles.Emaillist}>
+        {
+          emails.map(e => <Email />)
+        }
+        <Email />
+        <Email />
+     </ul>
+  )
+}
+
+export default Emaillist
