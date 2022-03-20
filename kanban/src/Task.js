16a13c133ddb2704c870c9473115c1c606ab56c1
Kim-JW <bbam03@naver.com>
2022-03-20 23:04:30 +0900

    done check

--- kanban/src/Task.js
+++ kanban/src/Task.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './assets/css/TaskList.css'
 
 const Task = ({name, done}) => {
+
+  const [checkState, setCheckState] = useState(done)
+
   return (
     <li className={styles.TaskList__Task}>
-                  <input type='checkbox' checked={done} />
-                  {name}
-                  <a href='#' className={styles.TaskList__Task__remove}></a>
-                </li>
+      <input 
+        type='checkbox' 
+        checked={checkState}
+        onClick={() => setCheckState(!checkState)} />
+      {name}
+      <a href='#' className={styles.TaskList__Task__remove}></a>
+    </li>
   )
 }
 
