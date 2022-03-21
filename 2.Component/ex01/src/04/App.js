7f0dc5ef77f5227add86db5f1fdbfb721e7ef232
Kim-JW <bbam03@naver.com>
2022-03-21 10:33:18 +0900

    Validation set, prop-types install

---
+++ 2.Component/ex01/src/04/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import MyComponent from './MyComponent.js';
+
+const App = function() {
+  
+    return (
+        <div id="App">
+          <MyComponent />
+        </div>
+  )
+}
+
+export default App
---
+++ 2.Component/ex01/src/04/MyComponent.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const MyComponent = () => {
+  return (
+    <Fragment>
+        <h2>Property Validation</h2>
+    </Fragment>
+  )
+}
+
+export default MyComponent
