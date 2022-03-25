a9180713f6be79b981f58d63844ccc023de84d1e
Kim-JW <bbam03@naver.com>
2022-03-25 09:56:35 +0900

    Lifecycle, useEffect, useState

--- 2.Component/ex05/src/04/App.js
+++ 2.Component/ex05/src/04/App.js
@@ -4,6 +4,34 @@ import Clock from './Clock';
 
 export default function App() {
 
+    const getCurrentClockTime = () => {
+        const date = new Date();
+        const hours = date.getHours();
+    
+        return {
+            hours: ('0' + (hours == 0 ? 12 : (hours > 12 ? hours-12 : hours))).slice(-2),
+            minutes: ('0' + date.getMinutes()).slice(-2),
+            seconds: ('0' + date.getSeconds()).slice(-2),
+            session: hours > 12 ? 'pm' : 'am',
+        }
+    }
+
+    const [currentClockTime, setCurrentClockTime] = useState(getCurrentClockTime);
+    const [ticks, setTicks] = useState(0);
+
+    // useEffect(() => {
+    //     setInterval(() => {
+    //         setCurrentClockTime(getCurrentClockTime());
+    //         setTicks(ticks+1);
+    //     });
+    // },[]);
+    
+    useEffect(() => {
+        setTimeout(() => {
+            setCurrentClockTime(getCurrentClockTime());
+            setTicks(ticks+1);
+        }, 1000);
+    },[currentClockTime]);
 
     return (
             <Clock
@@ -13,8 +41,8 @@ export default function App() {
                 minutes={state.minutes}
                 seconds={state.seconds}/>
                 */
-                hours={10}
-                minutes={25}
-                seconds={40}/>
+                hours={currentClockTime.hours}
+                minutes={currentClockTime.minutes}
+                seconds={currentClockTime.seconds}/>
     );
 }
